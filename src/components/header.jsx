d8ac537bde3731a4c8278f2cc24f6e5867702602
ematijasevic-conflu <ematijasevic@confluenciafto.com.ar>
2024-04-14 19:17:46 -0300

    primer commit

---
+++ src/components/header.jsx
@@ -0,0 +1,25 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">Contacto</a>
+          </li>
+          <li>
+            <a href="/product">Lotes</a>
+          </li>
+          <li>
+            <a href="/galery">Galeria</a>
+          </li>
+        </ul>
+      </nav>
+      <h1>Barrio Mandalas de Agua</h1>
+    </header>
+  );
+};
+
+export default Header;
